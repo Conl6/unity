8b2c5599404b5922a8ef9472e307e205f2da17d0
Conl6 <121958597+Conl6@users.noreply.github.com>
2023-03-09 11:55:52 +0000

    basic delivery system

---
+++ De Booty Hunters/Assets/DeliverSystem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeliverSystem : MonoBehaviour
+{
+    public Transform[] deliveryPoints; // holds delivery points
+    public float deliveryDistance = 2f; // distance at which a delivery is considered complete
+    public Text deliveryStatusText; // UI text to display delivery status
+    private Transform currentDeliveryPoint;
+    private bool hasDelivery;
+
+    private void Start()
+    {
+        SelectNewDeliveryPoint();
+    }
+
+    private void Update()
+    {
+        UpdateDeliveryStatusText();
+
+        if (hasDelivery && Vector3.Distance(transform.position, currentDeliveryPoint.position) < deliveryDistance)
+        {
+            CompleteDelivery();
+        }
+    }
+    private void SelectNewDeliveryPoint()
+    {
+        int index = Random.Range(0, deliveryPoints.Length);
+        currentDeliveryPoint = deliveryPoints[index];
+        hasDelivery = true;
+    }
+    private void CompleteDelivery()
+    {
+        hasDelivery = false;
+
+        SelectNewDeliveryPoint();
+    }
+    private void UpdateDeliveryStatusText()
+    {
+        if (hasDelivery)
+        {
+            deliveryStatusText.text = "Delivery: " + currentDeliveryPoint.name;
+        }
+        else
+        {
+            deliveryStatusText.text = "No active delivery";
+        }
+    }
+}
